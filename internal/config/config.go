f5cc795c324d9b437f92c92df7427bddec9c39f2
Федор Лебедев <flebedev777@gmail.com>
2024-12-03 21:47:53 +0000

    Feature/kafka

--- internal/config/config.go
+++ internal/config/config.go
@@ -6,8 +6,10 @@ import (
 )
 
 type KafkaConfig struct {
-	Listener string `env:"KAFKA_ADVERTISED_LISTENERS" env-default:"localhost"`
-	Host     string `env:"HOST_KAFKA" env-default:"localhost:9092"`
+	Topic        string `env:"TOPIC_NAME"`
+	SitesGroupID string `env:"SITES_GROUP_ID"`
+	Address      string `env:"KAFKA_ADDRESS"`
+	Partition    int    `env:"KAFKA_PARTITION"`
 }
 
 type ReceiverConfig struct {
