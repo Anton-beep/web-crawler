421a732a3d9f6a0517e3b1d75a202788c24a76ab
Anton-beep <anton.plyu@gmail.com>
2024-12-08 21:04:54 +0100

    fix

--- frontend/src/components/SitesGraph.tsx
+++ frontend/src/components/SitesGraph.tsx
@@ -1,9 +1,6 @@
 import { useEffect, useRef } from 'react';
 import ForceGraph3D from '3d-force-graph';
-import SpriteText from 'three-spritetext';
 import { GraphData } from '@/types/GraphData.ts';
-// import { UnrealBloomPass } from 'three/examples/jsm/postprocessing/UnrealBloomPass';
-import getDomainFromUrl from "../utils/getDomainFromUrl.ts";
 // @ts-expect-error : Cannot find module 'three/src/nodes/tsl/TSLCore'
 import {NodeObject} from "three/src/nodes/tsl/TSLCore";
 
@@ -16,7 +13,7 @@ export default function SitesGraph({width ,height, backgroundCol, data} : {width
             const Graph = ForceGraph3D()(graphRef.current)
                 .backgroundColor(backgroundCol)
                 .graphData(data)
-                .nodeLabel((node: NodeObject<number>) => getDomainFromUrl(node.id))
+                .nodeLabel('id')
                 .linkWidth(1)
                 .nodeAutoColorBy('id')
                 .width(width)
@@ -25,19 +22,12 @@ export default function SitesGraph({width ,height, backgroundCol, data} : {width
                 .linkDirectionalParticles(10)
                 .linkDirectionalParticleSpeed(0.003);
 
-            Graph.nodeThreeObject((node: NodeObject<number>) => {
-                const sprite = new SpriteText(node.id) as unknown as {color: string, textHeight: number, material: {depthWrite: boolean}};
-                sprite.material.depthWrite = false;
-                sprite.color = node.color;
-                sprite.textHeight = 8;
-                return sprite;
+            Graph.onNodeClick((node: NodeObject<number>) => {
+                const url = node.id;
+                if (url) {
+                    window.open(url, '_blank');
+                }
             });
-
-            // const bloomPass = new UnrealBloomPass();
-            // bloomPass.strength = 0.1;
-            // bloomPass.radius = 0;
-            // bloomPass.threshold = 0;
-            // Graph.postProcessingComposer().addPass(bloomPass);
 
             return () => {
                 Graph._destructor(); // Clean up on unmount
