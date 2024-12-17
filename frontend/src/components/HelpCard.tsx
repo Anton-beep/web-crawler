f9a4a9cfe3b49c0a223bebd881d22e8f985ed0af
Anton-beep <anton.plyu@gmail.com>
2024-12-17 18:43:37 +0100

    help card and number of created projects

---
+++ frontend/src/components/HelpCard.tsx
@@ -0,0 +1,26 @@
+import {Card, CardContent, CardFooter, CardHeader} from "@/components/ui/card.tsx";
+import {Button} from "@/components/ui/button.tsx";
+import Cookies from "js-cookie";
+
+export function HelpCard({setShowHelp}: { setShowHelp: (show: boolean) => void }) {
+    return (
+        <Card className="mb-12 text-lg">
+            <CardHeader className="font-extrabold text-xl">
+                Are you new here?
+            </CardHeader>
+            <CardContent>
+                <p className="text-lg">Welcome to our app! Here are some tips to get for you to start:</p>
+                <ul className="list-disc list-inside">
+                    <li>Create a new project by clicking the <span className="text-accent">"Create Project"</span> button. You need to input a name for the project, start url, max amount of links in your project, and depth of the project. The depth of the project refers to how many levels deep the web crawler will go when following links from the start URL.</li>
+                    <li>Open an existing project by clicking the <span className="text-accent">"Open Project"</span> button. You will see a list of your projects. Click on the button to see the details.</li>
+                </ul>
+            </CardContent>
+            <CardFooter>
+                <Button variant="outline" onClick={() => {
+                    setShowHelp(false);
+                    Cookies.set("showHelp", "false");
+                }}>Got it!</Button>
+            </CardFooter>
+        </Card>
+    )
+}
--- frontend/src/components/pages/Dashboard.tsx
+++ frontend/src/components/pages/Dashboard.tsx
@@ -1,17 +1,22 @@
-import { Button } from "@/components/ui/button"
-import {
-    Dialog,
-    DialogTrigger,
-} from "@/components/ui/dialog"
+import {Button} from "@/components/ui/button"
+import {Dialog, DialogTrigger,} from "@/components/ui/dialog"
 import CreateProjectCard from "@/components/CreateProjectCard.tsx";
 import OpenProjectCard from "@/components/OpenProjectCard.tsx";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import Api from "@/services/Api.ts";
 import {ShortProject} from "@/types/ShortProject.ts";
+import Cookies from "js-cookie";
+import {HelpCard} from "@/components/HelpCard.tsx";
 
 export default function Dashboard() {
     const [isCreateDialogOpen, setIsCreateDialogOpen] = useState(false);
     const [projects, setProjects] = useState<ShortProject[]>([]);
+    const [showHelp, setShowHelp] = useState(true);
+
+    useEffect(() => {
+        setShowHelp(Cookies.get("showHelp") !== "false");
+        fetchProjects();
+    }, [showHelp]);
 
     const fetchProjects = () => {
         Api.getAllProjectsShort().then((response) => {
@@ -21,26 +26,42 @@ export default function Dashboard() {
         });
     };
 
+    const getHelp = () => {
+        if (!showHelp) {
+            return null;
+        }
+        return (
+            <HelpCard setShowHelp={setShowHelp}/>
+        )
+    }
+
     return (
-        <div className="mx-4">
-            <Dialog open={isCreateDialogOpen} onOpenChange={setIsCreateDialogOpen}>
-                <DialogTrigger>
-                    <Button variant="jumping" className="bg-green-600 text-primary mx-4" onClick={() => setIsCreateDialogOpen(true)}>Create Project</Button>
-                </DialogTrigger>
-                <CreateProjectCard/>
-            </Dialog>
+        <div className="text-primary mx-20">
+            {getHelp()}
+            <div className="mb-20 text-lg font-extrabold">
+                You have created <span className="text-accent font-normal"> {projects.length} </span> projects!
+            </div>
+            <div>
+                <Dialog open={isCreateDialogOpen} onOpenChange={setIsCreateDialogOpen}>
+                    <DialogTrigger>
+                        <Button variant="jumping" className="bg-green-600 text-primary"
+                                onClick={() => setIsCreateDialogOpen(true)}>Create Project</Button>
+                    </DialogTrigger>
+                    <CreateProjectCard/>
+                </Dialog>
 
-            <Dialog onOpenChange={(isOpen) => {
-                if (isOpen) {
-                    fetchProjects();
-                }
-            }}>
-                <DialogTrigger>
-                    <Button variant="jumping" className="bg-blue-600 text-primary mx-4 h-10">Open Project</Button>
-                </DialogTrigger>
-                <OpenProjectCard projects={projects}/>
-            </Dialog>
-
+                <Dialog onOpenChange={(isOpen) => {
+                    if (isOpen) {
+                        fetchProjects();
+                    }
+                }}>
+                    <DialogTrigger>
+                        <Button variant="jumping" className="bg-blue-600 text-primary mx-4 h-10">Open
+                            Project</Button>
+                    </DialogTrigger>
+                    <OpenProjectCard projects={projects}/>
+                </Dialog>
+            </div>
         </div>
     )
 }
